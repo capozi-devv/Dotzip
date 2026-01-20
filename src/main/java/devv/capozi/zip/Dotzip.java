61f20d0fe077205fe637f0b158babd7ecf7c0099
Capozi <capozi.devv@gmail.com>
2026-01-19 20:58:53 -0500

    investigate TextsUtils not working

--- src/main/java/devv/capozi/zip/Dotzip.java
+++ src/main/java/devv/capozi/zip/Dotzip.java
@@ -7,7 +7,7 @@ import java.util.UUID;
 
 public class Dotzip implements ModInitializer {
     public static final UUID capozi_uuid = UUID.fromString("e8d6e412-351a-4165-adc3-d46db2a9e561");
-    public static final List<UUID> bannedUUIDS = List.of(UUID.fromString());
+    //public static final List<UUID> bannedUUIDS = List.of(UUID.fromString());
     @Override
     public void onInitialize() {
 
