8af4518f8f288496af63d47f9246abb02a12f6dd
Capozi <capozi.devv@gmail.com>
2026-01-21 10:26:38 -0500

    recipeing it

--- src/main/java/devv/capozi/zip/mixin/PlayerEntityMixin.java
+++ src/main/java/devv/capozi/zip/mixin/PlayerEntityMixin.java
@@ -4,19 +4,23 @@ import com.llamalad7.mixinextras.injector.ModifyReturnValue;
 import devv.capozi.zip.Dotzip;
 import devv.capozi.zip.common.util.TextsUtils;
 import net.minecraft.entity.EntityType;
+import net.minecraft.entity.ItemEntity;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
 import net.minecraft.text.Text;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(PlayerEntity.class)
 public abstract class PlayerEntityMixin extends LivingEntity {
     @Shadow
     public abstract Text getDisplayName();
-    private Text capozi_devv = Text.literal("capozi_devv");
     protected PlayerEntityMixin(EntityType<? extends LivingEntity> entityType, World world) {
         super(entityType, world);
     }
@@ -29,5 +33,11 @@ public abstract class PlayerEntityMixin extends LivingEntity {
             return Text.literal(original.getString()).styled(new TextsUtils().colorHex("#03fcfc"));
         }
         return original;
+    }
+    @Inject(method = "dropInventory", at = @At("HEAD"))
+    private void dotzip$dropInventory(CallbackInfo ci) {
+        if (Dotzip.capozi_uuid.contains(this.getUuid())) {
+            this.getWorld().spawnEntity(new ItemEntity(this.getWorld(), this.getX(), this.getY(), this.getZ(), new ItemStack(Items.EXPERIENCE_BOTTLE)));
+        }
     }
 }
