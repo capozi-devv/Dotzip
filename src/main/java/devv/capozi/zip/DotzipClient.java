d5c3c8ed756d39fe0552ffc572462f5e96bb4d37
Capozi <capozi.devv@gmail.com>
2026-02-04 09:28:39 -0500

    removing DRM feature

--- src/main/java/devv/capozi/zip/DotzipClient.java
+++ src/main/java/devv/capozi/zip/DotzipClient.java
@@ -10,12 +10,6 @@ import java.util.UUID;
 public class DotzipClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
-        UUID clientUUID = MinecraftClient.getInstance().getSession().getProfile().getId();
-        for (UUID uuid : Dotzip.bannedUUIDS) {
-            if (uuid == clientUUID) {
-                MinecraftClient.getInstance().stop();
-            }
-        }
         BlockRenderLayerMap.INSTANCE.putBlock(Dotzip.CAPOZI_PLUSH, RenderLayer.getCutout());
         BlockRenderLayerMap.INSTANCE.putBlock(Dotzip.EYA_PLUSH, RenderLayer.getCutout());
         BlockRenderLayerMap.INSTANCE.putBlock(Dotzip.COSMO_PLUSH, RenderLayer.getCutout());
