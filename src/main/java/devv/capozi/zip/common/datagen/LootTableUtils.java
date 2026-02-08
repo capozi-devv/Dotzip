4dc4341bf7902ff979e3a5d723dd01673e21dd77
Capozi <capozi.devv@gmail.com>
2026-02-07 20:51:55 -0500

    various updates

---
+++ src/main/java/devv/capozi/zip/common/datagen/LootTableUtils.java
@@ -0,0 +1,32 @@
+package devv.capozi.zip.common.datagen;
+
+import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
+import net.fabricmc.fabric.api.datagen.v1.provider.FabricBlockLootTableProvider;
+import net.minecraft.block.Block;
+import net.minecraft.block.DoorBlock;
+import net.minecraft.block.SlabBlock;
+import net.minecraft.block.TrapdoorBlock;
+
+import java.util.List;
+
+public class LootTableUtils extends FabricBlockLootTableProvider {
+    protected LootTableUtils(FabricDataOutput dataOutput) {
+        super(dataOutput);
+    }
+    @Override
+    public void generate() {
+
+    }
+
+    public void singleBlockLootTableFromList(List<Block> blocks) {
+        for(Block block : blocks) {
+            if (block instanceof SlabBlock) {
+                addDrop(block, slabDrops(block));
+            } else if (block instanceof DoorBlock) {
+                addDrop(block, doorDrops(block));
+            } else {
+                addDrop(block);
+            }
+        }
+    }
+}
--- src/main/java/devv/capozi/zip/common/datagen/RecipeUtils.java
+++ src/main/java/devv/capozi/zip/common/datagen/RecipeUtils.java
@@ -13,7 +13,7 @@ public class RecipeUtils extends FabricRecipeProvider {
     private RecipeUtils(FabricDataOutput output) {
         super(output);
     }
-    public void offerWoodSet(Consumer<RecipeJsonProvider> consumer, String modid, Ingredient woodIngredient, Block planks, SlabBlock slab, PressurePlateBlock pressurePlateBlock, ButtonBlock buttonBlock, DoorBlock doorBlock, FenceBlock fenceBlock, FenceGateBlock fenceGateBlock, StairsBlock stairsBlock, TrapdoorBlock trapdoorBlock) {
+    public static void offerWoodSet(Consumer<RecipeJsonProvider> consumer, String modid, Ingredient woodIngredient, Block planks, SlabBlock slab, PressurePlateBlock pressurePlateBlock, ButtonBlock buttonBlock, DoorBlock doorBlock, FenceBlock fenceBlock, FenceGateBlock fenceGateBlock, StairsBlock stairsBlock, TrapdoorBlock trapdoorBlock) {
         offerSlabRecipe(consumer, RecipeCategory.DECORATIONS, slab, planks);
         offerPressurePlateRecipe(consumer, pressurePlateBlock, planks);
         offerShapelessRecipe(consumer, buttonBlock, planks, modid, 1);
