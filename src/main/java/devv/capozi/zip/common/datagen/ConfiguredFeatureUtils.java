89f54f0fc9898e9e05cc5ec61ec1ee8cf0d444c6
Capozi <capozi.devv@gmail.com>
2026-02-09 19:06:22 -0500

    port to 1.21.1

--- src/main/java/devv/capozi/zip/common/datagen/ConfiguredFeatureUtils.java
+++ src/main/java/devv/capozi/zip/common/datagen/ConfiguredFeatureUtils.java
@@ -10,7 +10,7 @@ import net.minecraft.world.gen.feature.FeatureConfig;
 
 public class ConfiguredFeatureUtils {
     public static RegistryKey<ConfiguredFeature<?, ?>> registerKey(String mod_id, String name) {
-        return RegistryKey.of(RegistryKeys.CONFIGURED_FEATURE, new Identifier(mod_id, name));
+        return RegistryKey.of(RegistryKeys.CONFIGURED_FEATURE, Identifier.of(mod_id, name));
     }
     public static <FC extends FeatureConfig, F extends Feature<FC>> void register(Registerable<ConfiguredFeature<?, ?>> context, RegistryKey<ConfiguredFeature<?, ?>> key, F feature, FC configuration) {
         context.register(key, new ConfiguredFeature<>(feature, configuration));
--- src/main/java/devv/capozi/zip/common/datagen/RecipeUtils.java
+++ src/main/java/devv/capozi/zip/common/datagen/RecipeUtils.java
@@ -3,17 +3,23 @@ package devv.capozi.zip.common.datagen;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricRecipeProvider;
 import net.minecraft.block.*;
-import net.minecraft.data.server.recipe.RecipeJsonProvider;
+import net.minecraft.data.server.recipe.RecipeExporter;
 import net.minecraft.recipe.Ingredient;
 import net.minecraft.recipe.book.RecipeCategory;
+import net.minecraft.registry.RegistryWrapper;
 
+import java.util.concurrent.CompletableFuture;
 import java.util.function.Consumer;
 
 public class RecipeUtils extends FabricRecipeProvider {
-    private RecipeUtils(FabricDataOutput output) {
-        super(output);
+    private RecipeUtils(FabricDataOutput output, CompletableFuture<RegistryWrapper.WrapperLookup> registriesFuture) {
+        super(output, registriesFuture);
     }
-    public static void offerWoodSet(Consumer<RecipeJsonProvider> consumer, String modid, Ingredient woodIngredient, Block planks, SlabBlock slab, PressurePlateBlock pressurePlateBlock, ButtonBlock buttonBlock, DoorBlock doorBlock, FenceBlock fenceBlock, FenceGateBlock fenceGateBlock, StairsBlock stairsBlock, TrapdoorBlock trapdoorBlock) {
+    @Override
+    public void generate(RecipeExporter exporter) {
+
+    }
+    public static void offerWoodSet(RecipeExporter consumer, String modid, Ingredient woodIngredient, Block planks, SlabBlock slab, PressurePlateBlock pressurePlateBlock, ButtonBlock buttonBlock, DoorBlock doorBlock, FenceBlock fenceBlock, FenceGateBlock fenceGateBlock, StairsBlock stairsBlock, TrapdoorBlock trapdoorBlock) {
         offerSlabRecipe(consumer, RecipeCategory.DECORATIONS, slab, planks);
         offerPressurePlateRecipe(consumer, pressurePlateBlock, planks);
         offerShapelessRecipe(consumer, buttonBlock, planks, modid, 1);
@@ -22,10 +28,5 @@ public class RecipeUtils extends FabricRecipeProvider {
         createFenceGateRecipe(fenceGateBlock, woodIngredient);
         createStairsRecipe(stairsBlock, woodIngredient);
         createTrapdoorRecipe(trapdoorBlock, woodIngredient);
-    }
-
-    @Override
-    public void generate(Consumer<RecipeJsonProvider> consumer) {
-
     }
 }
