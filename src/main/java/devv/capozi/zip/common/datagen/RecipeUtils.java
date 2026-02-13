e0399c2f18fdb8d27d689d7d5a4b6d496e630418
Capozi <capozi.devv@gmail.com>
2026-02-12 21:57:49 -0500

    Update RecipeUtils.java

--- src/main/java/devv/capozi/zip/common/datagen/RecipeUtils.java
+++ src/main/java/devv/capozi/zip/common/datagen/RecipeUtils.java
@@ -4,8 +4,10 @@ import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricRecipeProvider;
 import net.minecraft.block.*;
 import net.minecraft.data.server.recipe.RecipeJsonProvider;
+import net.minecraft.item.Items;
 import net.minecraft.recipe.Ingredient;
 import net.minecraft.recipe.book.RecipeCategory;
+import net.minecraft.registry.tag.ItemTags;
 
 import java.util.function.Consumer;
 
@@ -17,11 +19,11 @@ public class RecipeUtils extends FabricRecipeProvider {
         offerSlabRecipe(consumer, RecipeCategory.DECORATIONS, slab, planks);
         offerPressurePlateRecipe(consumer, pressurePlateBlock, planks);
         offerShapelessRecipe(consumer, buttonBlock, planks, modid, 1);
-        createDoorRecipe(doorBlock, woodIngredient);
-        createFenceRecipe(fenceBlock, woodIngredient);
-        createFenceGateRecipe(fenceGateBlock, woodIngredient);
-        createStairsRecipe(stairsBlock, woodIngredient);
-        createTrapdoorRecipe(trapdoorBlock, woodIngredient);
+        createDoorRecipe(doorBlock, woodIngredient).criterion(hasItem(Items.OAK_PLANKS), conditionsFromTag(ItemTags.WOODEN_DOORS)).offerTo(consumer);
+        createFenceRecipe(fenceBlock, woodIngredient).criterion(hasItem(Items.OAK_PLANKS), conditionsFromTag(ItemTags.WOODEN_FENCES)).offerTo(consumer);
+        createFenceGateRecipe(fenceGateBlock, woodIngredient).criterion(hasItem(Items.OAK_PLANKS), conditionsFromTag(ItemTags.FENCE_GATES)).offerTo(consumer);
+        createStairsRecipe(stairsBlock, woodIngredient).criterion(hasItem(Items.OAK_PLANKS), conditionsFromTag(ItemTags.WOODEN_STAIRS)).offerTo(consumer);
+        createTrapdoorRecipe(trapdoorBlock, woodIngredient).criterion(hasItem(Items.OAK_PLANKS), conditionsFromTag(ItemTags.WOODEN_TRAPDOORS)).offerTo(consumer);
     }
 
     @Override
